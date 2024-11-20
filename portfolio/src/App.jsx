41f8678215ca4f7b450c431e9eb382632b94358e
dabby12 <liuweimi.sg@gmail.com>
2024-11-20 18:24:03 +0800

    added analytics

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import LogoGrid from './components/LogoGrid';
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
